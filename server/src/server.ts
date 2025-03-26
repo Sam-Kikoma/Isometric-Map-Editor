972cd76cfccd71e4f84cf9da727a98e0aa62cd25
Sam Kikoma <kikomasamson@gmail.com>
2025-03-26 07:27:32 +0100

    fix: JSON Body Parser Middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import cors from "cors";
 import router from "./router";
+import { protect } from "./modules/auth";
+import { createUser, signIn } from "./handlers/user";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", router);
+app.use("/api", protect, router);
 
-app.post("/user", (req, res) => {
-	res.send("Create new user");
-});
-app.post("/signin", (req, res) => {
-	res.send("Create new user");
-});
+app.post("/user", createUser);
+app.post("/signin", signIn);
 
 export default app;
