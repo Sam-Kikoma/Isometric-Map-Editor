5727d8d5d52df922557992b30069735af606235c
Sam Kikoma <kikomasamson@gmail.com>
2025-12-12 11:53:00 +0100

    feat: add editor components and types for modular structure

---
+++ client/src/components/editor/index.ts
@@ -0,0 +1,10 @@
+// Editor components
+export { default as SettingsPanel } from "./SettingsPanel";
+export { default as MapControls } from "./MapControls";
+export { default as Scene } from "./Scene";
+export { default as Cube } from "./Cube";
+export { default as Highlight } from "./Highlight";
+
+// Types and constants
+export { TEXTURES } from "./types";
+export type { CubeData } from "./types";
